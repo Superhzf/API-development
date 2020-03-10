46722c1c2edbc690dd7caf0fb2978aad4969a9d2
Superhzf <superhzf@hotmail.com>
2020-03-10 10:51:48 +0800

    type: feat
    Study security for FastAPI

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Query, Path, Body, Cookie, Header, Form, File, UploadFile, HTTPException,Request, Depends
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header, Form, File, UploadFile, HTTPException,Request, Depends,status
 from fastapi.responses import JSONResponse
 from fastapi.exception_handlers import http_exception_handler,request_validation_exception_handler
 from fastapi.encoders import jsonable_encoder
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from enum import Enum
 from typing import Optional, List, Set,Dict, Union
@@ -17,6 +18,9 @@ from datetime import datetime
 
 # app is an instance of the class FastAPI
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
 
 class ModelName(str,Enum):
     alexnet = 'alexnet'
@@ -311,4 +315,101 @@ async def verify_key(x_key:str=Header(...)):
 @app.get('/dependencies_path/', dependencies=[Depends(verify_key),Depends(verify_token)])
 async def read_items():
     return [{"item": "Foo"}, {"item": "Bar"}]
+
+
+# Security
+class User(BaseModel):
+    username:str
+    email: Optional[EmailStr]=None
+    full_name:Optional[str] = None
+    disabled:Optional[bool] = None
+
+
+def fake_decode_token(token):
+    return User(username=token+'fakedecoded',email='john@example.com',full_name='John Doe')
+
+
+async def get_current_user(token:str=Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+
+@app.get('/security/me')
+async def read_items(token:str=Depends(oauth2_scheme)):
+    return {'token':token}
+
+
+@app.get('/users/me')
+async def read_users_me(current_user:User=Depends(get_current_user)):
+    return current_user
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
+
+def fake_hash_password(password:str):
+    return 'fakehashed'+password
+
+
+class UserInDB2(User):
+    hashed_password:str
+
+def get_user(db,username:str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB2(**user_dict)
+
+def fake_decode_token2(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db,token)
+    return user
+
+
+async def get_current_user(token:str=Depends(oauth2_scheme)):
+    user = fake_decode_token2(token)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Invalid authentication credentials',
+                            headers={"WWW-Authenticate": "Bearer"})
+    return user
+
+
+async def get_current_active_user(current_user:User=Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail='Inactive user')
+    return current_user
+
+
+@app.post('/token')
+async def login(form_data:OAuth2PasswordRequestForm=Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail='Incorrent username')
+
+    user = UserInDB2(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if user.hashed_password != hashed_password:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail='Incorrect password')
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get('/users/me')
+async def read_users_me2(current_user:User=Depends(get_current_active_user)):
+    return current_user
 
