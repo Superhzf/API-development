fe844b3ee9917b786602e4dcbc055c56bb62309c
Superhzf <superhzf@hotmail.com>
2020-04-02 15:58:06 +0800

    type: feat
    Add alembic to do database migration and update docker files

--- dev-tools/service.docker-entrypoint.sh
+++ dev-tools/service.docker-entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#echo 'hello world'
+
 set -euo pipefail
 
-#DB=($(from_vault income-service database_host database_name database_username database_password))
-
-export POSTGRES_HOST=pgsql-dev
-export POSTGRES_DB=database
-export POSTGRES_USER=service
-export POSTGRES_PASSWORD=password
-export POSTGRES_PORT=${PGSQL_DEV_PORT}
+dockerize -wait "tcp://pgsql-dev:${PGSQL_DEV_PORT}"
+echo 'huzefunb'
+exec "$@"
